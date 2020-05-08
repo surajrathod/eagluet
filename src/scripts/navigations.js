16958f1c95691952cb2c58393c671cae40daea3b
surajrathod <surajrathod999@gmail.com>
2020-05-08 18:21:17 +0530

    changed to variable

--- src/scripts/navigations.js
+++ src/scripts/navigations.js
@@ -1,8 +1,24 @@
 const { remote } = require('electron');
+const DOMSECECTORES = {
+    'CLOSE': '.close__btn',
+    'MINIMIZE': '.minimize__btn',
+    "TIMER_SETTING": '.timer__btn',
+    "TIMER_SETTING_AREA": '.timer__setting__area'
+}
 
-document.querySelector('.close__btn').addEventListener('click', closeWindow);
-document.querySelector('.minimize__btn').addEventListener('click', minimizeWindow);
+document.querySelector(DOMSECECTORES.CLOSE).addEventListener('click', closeWindow);
+document.querySelector(DOMSECECTORES.MINIMIZE).addEventListener('click', minimizeWindow);
 
+const TimerBtn = document.querySelector(DOMSECECTORES.TIMER_SETTING)
+const SettingArea = document.querySelector(DOMSECECTORES.TIMER_SETTING_AREA);
+
+TimerBtn.addEventListener('click', toggleLeft);
+
+
+function toggleLeft(e) {
+    SettingArea.classList.toggle("--active_left")
+    TimerBtn.classList.toggle('--iconactive')
+}
 function closeWindow() {
     let window = remote.getCurrentWindow();
     window.close();
