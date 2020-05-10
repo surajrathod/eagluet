b1b83b5d9d3261d807acd3619ab0e7d4fec0e523
surajrathod <surajrathod999@gmail.com>
2020-05-10 12:13:25 +0530

    added eslint setup

--- src/scripts/navigations.js
+++ src/scripts/navigations.js
@@ -1,29 +1,34 @@
-const { remote } = require('electron');
-const DOMSECECTORES = {
-    'CLOSE': '.close__btn',
-    'MINIMIZE': '.minimize__btn',
-    "TIMER_SETTING": '.timer__btn',
-    "TIMER_SETTING_AREA": '.timer__setting__area'
+const { remote } = require("electron");
+
+const DOM_SELECTORS = {
+    CLOSE: document.querySelector(".close__btn"),
+    MINIMIZE: document.querySelector(".minimize__btn"),
+    TIMER_SETTING: document.querySelector(".timer__btn"),
+    TIMER_SETTING_AREA: document.querySelector(".timer-setting__area"),
+    CANCEL: document.querySelector(".cancel__btn"),
+    SAVE: document.querySelector(".save__btn"),
+    CLOCK_PLAYPAUSE: document.querySelector(".clock__play__pause")
 }
+//call the close window function
+DOM_SELECTORS.CLOSE.addEventListener("click", closeWindow);
 
-document.querySelector(DOMSECECTORES.CLOSE).addEventListener('click', closeWindow);
-document.querySelector(DOMSECECTORES.MINIMIZE).addEventListener('click', minimizeWindow);
+//call the minimizewindow window function
+DOM_SELECTORS.MINIMIZE.addEventListener("click", minimizeWindow);
 
-const TimerBtn = document.querySelector(DOMSECECTORES.TIMER_SETTING)
-const SettingArea = document.querySelector(DOMSECECTORES.TIMER_SETTING_AREA);
-
-TimerBtn.addEventListener('click', toggleLeft);
+DOM_SELECTORS.TIMER_SETTING.addEventListener("click", toggleLeft);
+DOM_SELECTORS.CANCEL.addEventListener("click", toggleLeft);
 
 
-function toggleLeft(e) {
-    SettingArea.classList.toggle("--active_left")
-    TimerBtn.classList.toggle('--iconactive')
+function toggleLeft() {
+    DOM_SELECTORS.TIMER_SETTING_AREA.classList.toggle("--active_left")
+    DOM_SELECTORS.TIMER_SETTING.classList.toggle("--iconactive")
 }
+//close the window when click on close button
 function closeWindow() {
     let window = remote.getCurrentWindow();
     window.close();
 }
-
+//minimize the window when click on minimize button
 function minimizeWindow() {
     let window = remote.getCurrentWindow();
     window.minimize();
