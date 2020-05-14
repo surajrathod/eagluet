07f78786d09ead10f027ba7de7f45aaf4bdab60f
surajrathod <surajrathod999@gmail.com>
2020-05-14 14:57:30 +0530

    blaa

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-require('electron-reload')(__dirname);
+require("electron-reload")(__dirname);
 
 app.allowRendererProcessReuse = true
 let mainWindow = null;
@@ -13,31 +13,32 @@ function createWindow() {
         frame: false,
         show: false,
         resizable: false,
+        alwaysOnTop: true,
         webPreferences: {
             nodeIntegration: true
         }
     }
     mainWindow = new BrowserWindow(windowsOptions);
-    mainWindow.loadURL(path.join('file://', __dirname, '/src/index.html'))
-    mainWindow.on('close', () => {
+    mainWindow.loadURL(path.join("file://", __dirname, "/src/index.html"))
+    mainWindow.on("close", () => {
         mainWindow = null;
     })
 
-    mainWindow.once('ready-to-show', () => {
+    mainWindow.once("ready-to-show", () => {
         mainWindow.show();
     })
 
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
     createWindow()
 })
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
         app.quit()
     }
 })
-app.on('activate', () => {
+app.on("activate", () => {
     if (mainWindow === null) {
         createWindow();
     }
