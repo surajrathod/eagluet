b464171a3cdbb54cf325692a5f9edc19a74d3d7a
surajrathod <surajrathod999@gmail.com>
2020-05-18 18:10:09 +0530

    added comments

--- src/js/Timer.js
+++ src/js/Timer.js
@@ -4,6 +4,7 @@ const { ipcRenderer } = require("electron");
 
 
 class Timer {
+    //will accept the session timer and current mode of play
     constructor(sessionMinute, mode) {
         this.sessionMinute = sessionMinute;
         this.playing = false;
@@ -13,45 +14,36 @@ class Timer {
     }
 
     counter() {
+
         if (!this.playing) {
             clearInterval(this.timerId);
             return
         }
-
+        //this will work for only initial 
+        //when the countdown is null
+        //this is for the functionality of pauseTimer
         if (this.countdown == null) {
             this.countdown = this.sessionMinute * 60 * 1000;
         }
-        console.log(this.countdown)
         this.timerId = setInterval(() => {
             if (this.playing == true) {
                 this.countdown -= 1000;
                 let min = Math.floor(this.countdown / (60 * 1000));
                 let sec = Math.floor((this.countdown - (min * 60 * 1000)) / 1000);
 
+                //countdown gows below 0
+                // stop the timer
+                // send message to the main process
                 if (this.countdown <= 0) {
                     this.stopTimer()
                     ipcRenderer.send("Countdown-Complete");
 
 
-                } else if (this.countdown === 54000) {
+                } else if (this.countdown === 5000) {
+                    //if counter has left with 5 second 
+                    // notify the user for that
                     this._renderMinuteSecond(min, sec);
-
-
-
-                    // let nextMode
-                    // switch (this.mode) {
-                    //     case "focus":
-                    //         nextMode = "break"
-                    //         break;
-                    //     case "break":
-                    //         nextMode = "focus"
-                    //         break;
-                    // }
-                    // notifier.notify({
-                    //     title: nextMode,
-                    //     message: "be prepared",
-                    //     sound: true,
-                    // })
+                    ipcRenderer.send("FiveSecondEarlyAlert", this.mode);
                 }
                 else {
                     this._renderMinuteSecond(min, sec);
@@ -62,16 +54,21 @@ class Timer {
         return
     }
     startTimer() {
-        this.playing = !this.playing
+        //change the playing to true call the counter
+        this.playing = true
         this.counter();
     }
 
     pauseTimer() {
-        this.playing = !this.playing;
+        //pause the timer
+        this.playing = false;
+
         clearInterval(this.timerId);
     }
 
     stopTimer() {
+        //stop the timer be clearing the timerid
+        //setting mull to countdown
         this.countdown = null;
         clearInterval(this.timerId)
         this.timerId = null;
