c4917c9a8d71d1c24b9c3702cfaaa91baa933e7a
surajrathod <surajrathod999@gmail.com>
2020-05-18 13:00:45 +0530

    Listen to counterCompler from main process

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+
 const path = require("path");
 
 require("electron-reload")(__dirname);
 
 app.allowRendererProcessReuse = true
 let mainWindow = null;
+
+
 
 function createWindow() {
     const windowsOptions = {
@@ -28,15 +31,29 @@ function createWindow() {
         mainWindow.show();
     })
 
+
+    // secondWindow = new BrowserWindow({ width: 500, height: 500, show: false, alwaysOnTop: true, frame: false, transparent: true })
+    // secondWindow.loadURL(path.join("file://", __dirname, "/src/focus.html"))
+    // secondWindow.on("close", () => {
+    //     secondWindow = null
+    // })
+
+
+
 }
+
+/**
+ * Listen for the 'Countdown-Complete' event from the 
+ * renderer process
+ * send the 'renderDefaultClock' to the renderer process
+ */
+
+ipcMain.on("Countdown-Complete", function (event) {
+    event.sender.send("renderDefaultClock");
+})
 
 app.on("ready", () => {
     createWindow()
-})
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit()
-    }
 })
 app.on("activate", () => {
     if (mainWindow === null) {
