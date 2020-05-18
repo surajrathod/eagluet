45b1851809ecf37e1724e348c51a7529ead23c0e
surajrathod <surajrathod999@gmail.com>
2020-05-18 18:10:25 +0530

    added comments

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
+const Notification = require("./src/js/Notification");
+
+
 
 const path = require("path");
 
@@ -8,8 +11,10 @@ app.allowRendererProcessReuse = true
 let mainWindow = null;
 
 
+let notification = new Notification();
 
 function createWindow() {
+
     const windowsOptions = {
         width: 450,
         height: 400,
@@ -51,6 +56,22 @@ function createWindow() {
 ipcMain.on("Countdown-Complete", function (event) {
     event.sender.send("renderDefaultClock");
 })
+
+/**
+ * All the Notification will be in main Process
+ */
+
+//this will we called to the notification class which will display the message
+ipcMain.on("FiveSecondEarlyAlert", function (event, mode) {
+    
+    notification.AlertFiveSecondEarly({
+        title: "Information",
+        mode: mode,
+        message: "will start in 5 sec",
+        icon: "rest.png",
+    });
+})
+
 
 app.on("ready", () => {
     createWindow()
